7fff826beab22f6b78ede5fbc407ef2ce1d9d66d
thomkok18 <tkok@roc-dev.com>
2016-07-14 10:14:59 +0200

    update

---
+++ JSP-Cursus/src/H3/Auto.java
@@ -0,0 +1,47 @@
+package H3;
+
+import java.text.DecimalFormat;
+
+public class Auto {
+    private final String merk;
+    private final String type;
+    private final int prijs;
+    private final String fotourl;
+    
+    
+    public Auto(String merk, String type, int prijs, String fotourl) {
+        this.merk = merk;
+        this.type = type;
+        this.prijs = prijs;
+        this.fotourl = fotourl;
+    }
+    
+    public String getMerk() {
+        return merk;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public int getPrijs() {
+    	
+        return prijs;
+    }
+    
+    public String getFoto() {
+        return fotourl;
+    }
+    
+    
+    /**
+      *
+      * @return geeft prijs als geformatteerde String
+      */
+    public String getPrijsFormat() {
+    	DecimalFormat df = new DecimalFormat("#,##0.--");
+    	double prijs = (double) this.getPrijs();
+    	String sPrijs = df.format(prijs);
+    	return sPrijs;
+    }
+}
---
+++ JSP-Cursus/src/H3/AutoLijst.java
@@ -0,0 +1,76 @@
+package H3;
+
+import java.util.ArrayList;
+
+/**
+ * Klasse met een lijst van auto's
+ * @author piet
+ */
+public class AutoLijst {
+    private ArrayList<Auto> autolijst;
+    
+    /**
+     * Constructor die de lijst aanmaakt en auto's toevoegt
+     */
+    public AutoLijst() {
+        autolijst = new ArrayList<Auto>();
+        Auto ford1 = new Auto("Ford", "Fiesta", 23999, 
+          "images/autos/fordfiesta.jpg");
+        autolijst.add(ford1);
+        Auto ford2 = new Auto("Ford", "Focus", 18995, 
+          "images/autos/fordfocus.jpg");
+        autolijst.add(ford2);
+        Auto opel1 = new Auto("Opel", "Astra", 14895, 
+          "images/autos/opelastra.jpg");
+        autolijst.add(opel1);
+        Auto opel2 = new Auto("Opel", "Insignia", 65450, 
+          "images/autos/opelinsignia.jpg");
+        autolijst.add(opel2);
+        Auto subaru1 = new Auto("Subaru", "Forester", 34750, 
+          "images/autos/subaruforester.jpg");
+        autolijst.add(subaru1);
+        Auto subaru2 = new Auto("Subaru", "Impreza", 28345, 
+          "images/autos/subaruimpreza.jpg");
+        autolijst.add(subaru2);
+        Auto mercedes1 = new Auto("Mercedes", "E 63", 110995, 
+          "images/autos/mercedede63.png");
+        autolijst.add(mercedes1);
+        Auto mercedes2 = new Auto("Mercedes", "CLA 45", 190780, 
+          "images/autos/mercedescla45.jpg");
+        autolijst.add(mercedes2);
+        Auto ferrari1 = new Auto("Ferrari", "612 GTO", 350500, 
+          "images/autos/ferrari612.jpg");
+        autolijst.add(ferrari1);
+        Auto citroen1 = new Auto("Citroen", "2CV", 459, 
+          "images/autos/citroen2cv.jpg");
+        autolijst.add(citroen1);
+        Auto mini1 = new Auto("Mini", "Cooper", 34495, 
+          "images/autos/minicooper.jpg");
+        autolijst.add(mini1);
+     }
+    
+    /**
+     * 
+     * @return geeft de lijst met auto's
+     */
+    public ArrayList<Auto> getLijst() {
+        return autolijst;
+    }
+    
+    
+    
+    /**
+     * Ten behoeve van het select merk input veld
+     * @return een lijst met alle unieke merken
+     */
+    public ArrayList<String> getMerken() {
+        ArrayList<String> merken = new ArrayList<String>();
+        for (Auto auto: autolijst) {
+            String merk = auto.getMerk();
+            if (!merken.contains(merk)) {
+                merken.add(merk);
+            }
+        }
+        return merken;
+    }
+}
