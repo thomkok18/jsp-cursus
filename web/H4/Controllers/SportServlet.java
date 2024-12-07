3bc97485eea75fa7aa13612a20ff8f0644379b6f
Thomk <thomkok13@hotmail.com>
2024-12-07 12:58:07 +0100

    Servlet added.

---
+++ web/H4/Controllers/SportServlet.java
@@ -0,0 +1,10 @@
+public class SportServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //deze methode gaat alle get requests afhandelen
+    }
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //deze methode handelt alle post requests af.
+    }
+}
